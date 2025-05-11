7265fc63909bc04e3bb273e1d3521a13916ffad6
Fernandaqm07 <fernandaquinteromartinez26@gmail.com>
2025-05-11 10:50:33 -0500

    Add files via upload

---
+++ GA7-220501096-AA3-EV01/src/main/java/com/fernanda/cotizacionapp/model/CotizacionAppApplication.java
@@ -0,0 +1,13 @@
+package com.fernanda.cotizacionapp.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotizacionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CotizacionAppApplication.class, args);
+    }
+}
+
